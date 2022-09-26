b15e785641598dd181025504b94450b64f60de28
Abadonna13 <i@Abadonna13.ru>
2022-09-26 11:22:08 +0300

    task13 Done

--- task13/Program.cs
+++ task13/Program.cs
@@ -3,3 +3,19 @@
 // 645 -> 5
 // 78 -> третьей цифры нет
 // 32679 -> 6
+
+Console.WriteLine("Введите число");
+int N = Convert.ToInt32(Console.ReadLine());
+
+if (N <100)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+else
+{
+    while(N >= 1000)
+    {
+      N = N / 10;  
+    }
+    Console.WriteLine($"Третье число {N % 10}");    
+    }
