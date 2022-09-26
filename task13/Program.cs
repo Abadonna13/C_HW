3e867caebde55090e22e5dc65bb3c9d6da1ec352
Abadonna13 <i@Abadonna13.ru>
2022-09-26 11:29:22 +0300

    task15 Done

--- task13/Program.cs
+++ task13/Program.cs
@@ -7,15 +7,15 @@
 Console.WriteLine("Введите число");
 int N = Convert.ToInt32(Console.ReadLine());
 
-if (N <100)
+if (N < 100)
 {
     Console.WriteLine("Третьей цифры нет");
 }
 else
 {
-    while(N >= 1000)
+    while (N >= 1000)
     {
-      N = N / 10;  
+        N = N / 10;
     }
-    Console.WriteLine($"Третье число {N % 10}");    
-    }
+    Console.WriteLine($"Третье число {N % 10}");
+}
--- task15/Program.cs
+++ task15/Program.cs
@@ -4,3 +4,22 @@
 // 6 -> да
 // 7 -> да
 // 1 -> нет
+
+Console.WriteLine("Введите число");
+int N = Convert.ToInt32(Console.ReadLine());
+
+if (N < 1 || N > 7)
+{
+    Console.WriteLine("Нет такого дня недели!");
+}
+else
+{
+    if (N == 6 || N == 7)
+    {
+        Console.WriteLine("Ваходной");
+    }
+    else
+    {
+        Console.WriteLine("Рабочий");
+    }
+}
