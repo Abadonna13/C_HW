cce690b7ba7d6ff33db49e76c9cf59cec4c8449d
Abadonna13 <i@Abadonna13.ru>
2022-09-26 18:25:55 +0300

    task23 done

--- task23/Program.cs
+++ task23/Program.cs
@@ -5,3 +5,17 @@
 // 3 -> 1, 8, 27
 // 5 -> 1, 8, 27, 64, 125
 
+Console.WriteLine("Введите число N");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (n > 0)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        Console.Write(i * i * i + " ");
+    }
+}
+else
+{
+    Console.WriteLine("N должно быть больше 0!");
+}
