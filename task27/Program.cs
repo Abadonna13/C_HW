e6ee1fd2db1a4b6c98ec007a7af3945a5f8d07be
Abadonna13 <i@Abadonna13.ru>
2022-09-30 08:16:15 +0300

    task27 done

--- task27/Program.cs
+++ task27/Program.cs
@@ -3,3 +3,25 @@
 // 452 -> 11
 // 82 -> 10
 // 9012 -> 12
+
+int ReadNum(string mess)
+{
+    Console.WriteLine(mess);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int GetSumOfDigits(int num)
+{
+    int sum = 0;
+    if (num == 0) sum = 0;
+    while (num != 0)
+    {
+        sum = sum + num % 10;
+        num = num / 10;
+    }
+    return sum;
+}
+
+int number = ReadNum("Введите число:");
+int sum = GetSumOfDigits(number);
+Console.WriteLine(sum);
