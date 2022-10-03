4a0ade3c42c030caba7fcdd533be0dda41dd31da
Abadonna13 <i@Abadonna13.ru>
2022-10-03 16:37:14 +0300

    task36 done

--- task36/Program.cs
+++ task36/Program.cs
@@ -4,3 +4,32 @@
 // [3, 7, 23, 12] -> 19
 // [-4, -6, 89, 6] -> 0
 
+int[] GetArray(int size, int leftRange, int rightRange)
+{
+    int[] arr = new int[size];
+    Random rand = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = rand.Next(leftRange, rightRange + 1);
+    }
+    return arr;
+}
+
+int SumOddPositionElements(int[] arr)
+{
+    int Sum = 0;
+    for (int i = 1; i < arr.Length; i += 2)
+    {
+
+        Sum += arr[i];
+
+    }
+    return Sum;
+}
+
+Random rand = new Random();
+int randomSize = rand.Next(5, 20);
+int[] array = GetArray(randomSize, 0, 100);
+Console.WriteLine(string.Join(", ", array));
+int SumOdd = SumOddPositionElements(array);
+Console.WriteLine($"Сумма элементов, стоящих на нечётных позициях = {SumOdd}");
