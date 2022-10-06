e55637cb56b3349a4bab09ede42e1e5bd5a75e17
Abadonna13 <i@Abadonna13.ru>
2022-10-06 18:32:58 +0300

    task43 done

--- task43/Program.cs
+++ task43/Program.cs
@@ -5,25 +5,21 @@
 // b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 
 // Запрос числа у пользователя
-int ReadNum(string mess)
+double ReadNum(string mess)
 {
     Console.WriteLine(mess);
     return Convert.ToInt32(Console.ReadLine());
 }
 
 
-int b1 = ReadNum("Введите b1:");
-int k1 = ReadNum("Введите k1:");
-int b2 = ReadNum("Введите b2:");
-int k2 = ReadNum("Введите k2:");
+double  b1 = ReadNum("Введите b1:");
+double  k1 = ReadNum("Введите k1:");
+double  b2 = ReadNum("Введите b2:");
+double  k2 = ReadNum("Введите k2:");
 
 if(k1 != k2)
 {
-    //double x = (b2-b1)/(k1-k2);
-    double x = 2/(-4);
-    Console.WriteLine (b2-b1);
-    Console.WriteLine (k1-k2);
-    Console.WriteLine (x);
+    double x = (b2-b1)/(k1-k2);
     double y = k1 * x + b1;
     Console.WriteLine($"Точка пересечения ({x},{y})");
 }
